9df1e6e4e7c1b953af0ef7a9e112461d997fa980
levsthings <bearnode@gmail.com>
2017-08-31 03:52:07 +0300

    add error handling and terminate connections on error paths

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 exports.parseJson = (source, encoding) => JSON.parse((fs.readFileSync(source, encoding)))
-exports.log = console.log
 exports.logError = message => console.log(chalk.red(`Error: ${message}\n`))
 exports.logSuccess = message => console.log(chalk.green(`Success: ${message}\n`))
 exports.logNotify = message => console.log(chalk.yellow(`Notification: ${message}\n`))
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 'use strict'
 const R = require('ramda')
 const WebSocket = require('ws')
+const {logError, logNotify} = require('../scripts/utils/')
 const {stream} = require('./streams')
 const {client} = require('./vendor/twitter')
 const {validateMessage} = require('./validateMessage')
 
 const wss = new WebSocket.Server({ port: 8080 })
 
-// TODO: Block multiple connections
 wss.on('connection', (ws) => {
     ws.on('message', (msg) => {
         const isValid = validateMessage(msg)
+
         if (!isValid) {
-            ws.send('Invalid request')
-            throw new Error('Invalid message from client')
-            // TODO: Close connection
+            ws.terminate()
         } else {
             const {tags} = isValid
             let trackedData = R.fromPairs(R.map(key => [key, 0], tags))
@@ -28,9 +27,19 @@ wss.on('connection', (ws) => {
                 })
 
                 hashtagStream.on('error', (error) => {
-                    // TODO: Send an error object
-                    throw error
+                    logError('Twitter Stream Error', error)
+                    hashtagStream.destroy()
+                    ws.terminate()
                 })
+            })
+
+            ws.on('close', () => {
+                logNotify('Connection closed')
+                ws.terminate()
+            })
+            ws.on('error', (error) => {
+                logError('Error caught', error)
+                ws.terminate()
             })
         }
     })
